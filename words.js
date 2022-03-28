d625452878344c014f5ffab398dc7be91046740c
Julian Garcia <69781917+julianagar@users.noreply.github.com>
2022-03-27 22:43:46 -0700

    initial

---
+++ words.js
@@ -0,0 +1,17 @@
+// if you're here cheating:
+// good for you honestly/
+
+export const WORDS = [
+  'james',
+  'logan',
+  'mason',
+  'jacob',
+  'lucas',
+  'ethan',
+  'aiden',
+  'henry',
+  'akira',
+  'alana',
+  'abdul',
+  'april'
+]
